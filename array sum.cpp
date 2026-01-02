90bf7e72aa5d4a55e389f27ed5dd258e7a1b3b0a
Amrita shajikumar <amritashajikumar@gmail.com>
2026-01-03 00:55:04 +0530

    Add files via upload

---
+++ array sum.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int,int> mp;
+    for(int i = 0; i < nums.size(); i++) {
+        int rem = target - nums[i];
+        if(mp.count(rem)) {
+            return {mp[rem], i};
+        }
+        mp[nums[i]] = i;
+    }
+    return {};
+}
